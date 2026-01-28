230bc11cb5c505b37740238844868d7fbb38a6a3
naveen17b <bnaveen1794@gmail.com>
2026-01-28 11:45:28 +0530

    test to check the infinite scroll functionality

---
+++ tests/infiniteScrolling.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect, Locator } from "@playwright/test";
+import { log } from "node:console";
+
+test("Scrolling inside dropdown", async ({ page }) => {
+  test.slow();
+  await page.goto("https://www.booksbykilo.in/new-books?pricerange=201to500");
+  await page.waitForLoadState("load");
+  //scrolling infinite scrolling
+  let previousHeight = 0;
+  while (true) {
+    await page.evaluate(() => {
+      window.scrollBy(0, document.body.scrollHeight);
+    });
+
+    await page.waitForTimeout(2000);
+    const lastHeight = await page.evaluate(() => {
+      return document.body.scrollHeight;
+    });
+
+    console.log(previousHeight);
+    console.log(lastHeight);
+
+    if (lastHeight === previousHeight) {
+          break;
+    }
+    previousHeight = lastHeight;
+  }
+
+  await page.waitForTimeout(3000);
+});
